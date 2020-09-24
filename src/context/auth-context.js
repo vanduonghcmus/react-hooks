ad76cfb936ee42fcab3b1663bb030c15639467d4
vanduonghcmus <58739835+vanduonghcmus@users.noreply.github.com>
2020-09-24 18:56:08 +0700

    working with useContext

---
+++ src/context/auth-context.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+
+export const AuthContext = createContext({
+  isAuth: false,
+  login: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const loginHandler = () => {
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ login: loginHandler, isAuth: isAuthenticated }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
