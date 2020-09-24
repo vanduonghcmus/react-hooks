5ebeaf34e66d57c31ae12a4cdc9524f93c3f9042
vanduonghcmus <58739835+vanduonghcmus@users.noreply.github.com>
2020-09-24 18:03:49 +0700

    use useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,53 +1,95 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useReducer } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
+
+const ingredientReducer = (currentIngredient, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredient, action.ingredient];
+    case "DELETE":
+      return currentIngredient.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error("Should not get there");
+  }
+};
 
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     console.log("Rendering Ingredients", userIngredients);
   }, [userIngredients]);
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
     fetch("https://react-hook-update-6a174.firebaseio.com/ingredients.json", {
       method: "POST",
       body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" },
     })
       .then((response) => {
+        setIsLoading(false);
         return response.json();
       })
       .then((responseData) => {
-        setUserIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient },
-        ]);
+        // setUserIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient },
+        // ]);
+        dispatch({
+          type: "ADD",
+          ingredient: { id: responseData.name, ...ingredient },
+        });
       });
   };
 
   const filteredIngredientHandler = useCallback((filteredIngredient) => {
-    setUserIngredients(filteredIngredient);
+    // setUserIngredients(filteredIngredient);
+    dispatch({ type: "SET", ingredients: filteredIngredient });
   }, []);
 
   const removeIngredientHandler = (ingredientId) => {
+    setIsLoading(true);
     fetch(
       `https://react-hook-update-6a174.firebaseio.com/ingredients/${ingredientId}.json`,
       {
         method: "DELETE",
       }
-    ).then((response) => {
-      setUserIngredients((prevIngredient) =>
-        prevIngredient.filter((ingredients) => ingredients.id !== ingredientId)
-      );
-    });
+    )
+      .then((response) => {
+        setIsLoading(false);
+        // setUserIngredients((prevIngredient) =>
+        //   prevIngredient.filter(
+        //     (ingredients) => ingredients.id !== ingredientId
+        //   )
+        // );
+        dispatch({ type: "DELETE", id: ingredientId });
+      })
+      .catch((error) => {
+        setError("Something went wrong!");
+        setIsLoading(false);
+      });
+  };
+
+  const closeError = () => {
+    setError(null);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={closeError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
       <section>
         <Search onLoadingsIngredient={filteredIngredientHandler} />
         <IngredientList
